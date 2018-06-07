5b663e0be34d1e24f8f582132d3e67a08ce87bba
trilokeshraom <trilokeshrao@gmail.com>
2018-06-07 13:15:26 +0930

    installing bootstrap

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,6 @@
 require 'pry'
 class ArticlesController < ApplicationController
+  before_action :set_article, only:[:show,:edit,:update,:destroy]
 
   def new
     @article = Article.new
@@ -7,7 +8,6 @@ class ArticlesController < ApplicationController
 
   #The controller would come here when we click on submit in the new form
   def create
-    #binding.pry
     #render plain: params[:article].inspect
     @article = Article.new(article_params)
     if @article.save
@@ -18,20 +18,18 @@ class ArticlesController < ApplicationController
     end
   end
 
+
   #This is the action which gets invoked everytime some display is placed in the screen like after creation or edit
   def show
-    @article = Article.find(params[:id])
   end
+
 
   def edit
-    #binding.pry
-    @article = Article.find(params[:id])
   end
 
+
   def update
-    #binding.pry
     #You are calling the article from the database with the article id
-    @article = Article.find(params[:id])
     #Then updating it
     if @article.update(article_params)
       flash[:notice] = "The article was updated"
@@ -41,19 +39,25 @@ class ArticlesController < ApplicationController
     end
   end
 
+
   def index
     #To note here that we are using the instance variable of @articles instead of @article. You can use anything. I am using @articles
     @articles = Article.all
   end
 
+
   def destroy
-    @article = Article.find(params[:id])
     @article.destroy
     flash[:notice] = "The article was sucessfully destroyed"
     redirect_to articles_path
   end
 
+
   private
+
+  def set_article
+    @article = Article.find(params[:id])
+  end
 
   def article_params
     params.require(:article).permit(:name, :description)
