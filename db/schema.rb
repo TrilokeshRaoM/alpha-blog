f2601799c56a1abd7fefcb3da224c7d3bde38a59
trilokeshraom <trilokeshrao@gmail.com>
2018-06-13 21:41:49 +0930

    Write validation for user object

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_06_05_124252) do
+ActiveRecord::Schema.define(version: 2018_06_07_043607) do
 
   create_table "articles", force: :cascade do |t|
     t.string "name"
     t.text "description"
     t.datetime "updated_at"
     t.datetime "created_at"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.string "email"
   end
 
 end
---
+++ vendor/gems/ffi-1.9.23/ext/ffi_c/libffi-x86_64-linux/include/ffitarget.h
@@ -0,0 +1,1 @@
+/home/tri/workspace/alpha-blog/vendor/gems/ffi-1.9.23/ext/ffi_c/libffi/src/x86/ffitarget.h
